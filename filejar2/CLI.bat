ea3acdd5952918f2f089483897161a77d714aea9
gpamaa <142616082+gpamaa@users.noreply.github.com>
2025-01-30 12:26:07 +0100

    Delete filejar2 directory

--- filejar2/CLI.bat
+++
@@ -1,4 +0,0 @@
-@ECHO OFF
-start java -jar src\CLI\server.jar
-@ECHO OFF
-start java -jar src\CLI\client.jar localhost 1010
--- filejar2/GUI.bat
+++
@@ -1,4 +0,0 @@
-@ECHO OFF
-start java -jar src\GUI\server.jar
-@ECHO OFF
-start javaw  --module-path "lib\javafx-sdk-19\lib" --add-modules javafx.controls,javafx.fxml -jar src\GUI\Client.jar
